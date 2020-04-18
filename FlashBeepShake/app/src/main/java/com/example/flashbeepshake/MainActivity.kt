4391fddb1e1eda8d4f87e9e6198b8f8002b2f248
austingailey <agailey96@gmail.com>
2020-04-18 13:27:44 -0600

    Adding FlashBeepShake

---
+++ FlashBeepShake/app/src/main/java/com/example/flashbeepshake/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.example.flashbeepshake
+
+import android.content.Context
+import android.hardware.camera2.CameraManager
+import android.media.AudioManager
+import android.media.ToneGenerator
+import android.os.Bundle
+import android.os.Vibrator
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var flashOnOff:Boolean = false;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+        flashBtn.setOnClickListener{
+            flash()
+        }
+        beepBtn.setOnClickListener{
+            beep()
+        }
+        shakeBtn.setOnClickListener {
+            shake()
+        }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    fun flash(){
+        val cm:CameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        val camera = cm.cameraIdList[0]
+
+        if(flashOnOff) {
+            cm.setTorchMode(camera, false)
+            flashOnOff = false
+        }else{
+            cm.setTorchMode(camera, true)
+            flashOnOff = true
+        }
+    }
+
+    fun beep(){
+        val tone = ToneGenerator(AudioManager.STREAM_MUSIC, 100)
+        tone.startTone(ToneGenerator.TONE_DTMF_3,600) //play specific tone for 600ms
+    }
+
+    fun shake() {
+        val vibrator:Vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        vibrator.vibrate(1000)
+    }
+}
